2410a9ba887568cecc1be9aeef5b46842df4069c
Oladipo Adesiyan <adesiyanoladipo1@gmail.com>
2022-10-27 10:06:40 +0100

    Ref: Switched to MongoDB

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,17 +1,16 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
+import * as dotenv from "dotenv"
 import { User } from "./entity/User"
 
+require('dotenv').config({ path: __dirname + '/.env'})
+
 export const AppDataSource = new DataSource({
-    type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "test",
-    password: "test",
-    database: "test",
+    type: "mongodb",
+    url: process.env.MONGO_URI,
+    useNewUrlParser: true,
     synchronize: true,
     logging: false,
     entities: [User],
-    migrations: [],
     subscribers: [],
 })
